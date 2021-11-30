352f357e912e4badbe8cd8cc6edbc22484d79ca1
Philip <philipmjobe@gmail.com>
2021-11-30 14:52:19 -0500

    added table.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, React!</h1>
-      </div>
-    );
-  }
-}
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
